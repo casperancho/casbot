4b43514c61c16d98acdd9747800e019ba59a6b4b
Artem <33518804+casperancho@users.noreply.github.com>
2018-04-23 23:10:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,9 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+    who = message.from_user.first_name
+    bot.send_message(message.chat.id, text=' Hello {who}'.format(who))
+    #    bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
