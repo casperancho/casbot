9a92b1134f1599aebd701181710af85607888ebb
Артем Закиров <Zakirov@MacBook-Pro-Artem.local>
2018-04-24 12:11:01 +0300

    try3

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, 'привет, {who}'.format(who=message.from_user.first_name))
+    bot.send_message(message.chat.id, 'привет, {who} !'.format(who=message.from_user.first_name))
 
 
 
