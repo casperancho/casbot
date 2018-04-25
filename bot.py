fb50ae3085b7e2f6ee72989e3023de7c30912455
Artem <33518804+casperancho@users.noreply.github.com>
2018-04-25 21:11:46 +0300

    Update bot.py
    Add show

--- bot.py
+++ bot.py
@@ -3,11 +3,15 @@ import config
 import telebot
 
 bot = telebot.TeleBot(config.token)
-
+allmes=[]
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, 'привет, {who} !'.format(who=message.from_user.first_name))
-
+    if message.text == "show":
+        bot.send_message(message.chat.id, allmes)
+    else:
+        bot.send_message(message.chat.id, 'привет, {who} {fam}!'.format(who=message.from_user.first_name,fam=message.from_user.last_name))
+        allmes.append(message.text)
+    
 
 
 if __name__ == '__main__':
