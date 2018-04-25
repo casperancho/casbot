dd5b1b6f97e46dd716018ea9fd41e3791cab8b54
Artem <33518804+casperancho@users.noreply.github.com>
2018-04-25 22:56:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,11 @@ allmes=[]
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     if message.text == "show":
         bot.send_message(message.chat.id, allmes)
+    else if message.text=="Настя":
+        bot.send_message(message.chat.id, ' {who} {fam}!, твой костю топ, Ты просто секс'.format(who=message.from_user.first_name,fam=message.from_user.last_name))
+       
     else:
-        bot.send_message(message.chat.id, 'привет, {who} {fam}!'.format(who=message.from_user.first_name,fam=message.from_user.last_name))
+        bot.send_message(message.chat.id, 'привет, {who} {fam}!, твой костю топ'.format(who=message.from_user.first_name,fam=message.from_user.last_name))
         allmes.append(message.text)
     
 
